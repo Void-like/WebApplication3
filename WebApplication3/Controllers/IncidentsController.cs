1bf14bca4550a04b9d4aa5419e2a860405b2eb94
student <student@CAB4-COMP8>
2025-11-07 16:07:44 +1000

    2

--- WebApplication3/Controllers/IncidentsController.cs
+++ WebApplication3/Controllers/IncidentsController.cs
@@ -8,32 +8,59 @@ using System.Security.Claims;
 
 namespace WebApplication3.Controllers
 {
-        [Authorize]
-        [Route("api/[controller]")]
-        [ApiController]
-        public class IncidentsController : ControllerBase
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncidentsController : ControllerBase
+    {
+        private readonly ItCompany1135Context db;
+
+        public IncidentsController(ItCompany1135Context db)
         {
-            private readonly ItCompany1135Context db;
-
-            public IncidentsController(ItCompany1135Context db)
-            {
-                this.db = db;
-            }
-         
-            [HttpPost("Report")]
-            public  ActionResult AddIncident()
-            {
-                var client = GetClient();
-                if(client == null)
-                    return Forbid();
-              //  var incident = GetIncident(client.id);
-              
-                db.SaveChanges();
-                return Ok();
-            }
-
-
+            this.db = db;
         }
+
+        [HttpPost("Report")]
+        public ActionResult AddIncident()
+        {
+            var client = GetClient();
+            if (client == null)
+                return Forbid();
+            //  var incident = GetIncident(client.id);
+
+            db.SaveChanges();
+            return Ok();
+        }
+
+
+
+        Client? GetClient()
+        {
+            var claim = User.Claims.First();
+            if (claim.Type != ClaimValueTypes.Sid)
+                return null;
+
+            var client = db.Clients.Find(claim.Value);
+            if (client == null)
+                return null;
+
+            return client;
+        }
+        Incident? GetIncident(string clientId)
+        {
+            var incident = db.Incidents.Find();
+
+
+            if (incident == null)
+            {
+
+            }
+            return incident;
+            db.Incidents.Add(incident);
+            db.SaveChanges();
         }
     }
+}
+
+    
 
